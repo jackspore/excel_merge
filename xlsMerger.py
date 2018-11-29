7f84fe5e0e485f976ac42838295485f5094dfa1e
jackspore <sporeyang@live.cn>
2018-11-29 20:38:06 +0800

    take parameters by user input

--- xlsMerger.py
+++ xlsMerger.py
@@ -7,6 +7,8 @@ cwd = os.getcwd()
 
 # idxSheet = int(sys.argv[1])
 # idxColumn = int(sys.argv[2])
+idxSheet = int(input('Number of sheet to read from each file:')) -1
+idxColumn = int(input('Number of column to read from each sheet:')) -1
 
 idxWr = 0
 outputFilename = 'merged_file.xls'
@@ -14,13 +16,13 @@ outFile = xlwt.Workbook('UFT-8')
 sheet0 = outFile.add_sheet('Sheet1', False)
 
 listFiles = os.listdir(cwd) # list out all files under working folder
-for xls in listFiles:
-    # print(xls)
-    filename, fileext = os.path.splitext(xls)
+for fl in listFiles:
+    print('processing file',fl, '...')
+    filename, fileext = os.path.splitext(fl)
     # print(filename)
     # print(fileext)
     if (fileext == '.xls' or fileext == '.xlsx'):
-        workbook = xlrd.open_workbook(xls) # open excel file
+        workbook = xlrd.open_workbook(fl) # open excel file
         sheet = workbook.get_sheet(0) # get first sheet
         col = sheet.col_values(0) # get first column
 
