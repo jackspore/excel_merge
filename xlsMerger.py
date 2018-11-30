1a1a59604f094c80ffac65b820fd90ed2654b91e
jackspore <sporeyang@live.cn>
2018-11-30 23:48:17 +0800

    format the code

--- xlsMerger.py
+++ xlsMerger.py
@@ -13,16 +13,21 @@ import  time
 from  datetime  import  date 
 
 wdir = input('Folder path holds excel files:')
-
-rowsWr = 0 # how many rows written
 outputFilename = os.path.join(wdir, 'merged_file.xls')
 if os.path.isfile(outputFilename):
     os.remove(outputFilename) # remove output file if already exist
+
 outFile = xlwt.Workbook('UFT-8')
+rmbStyle = xlwt.Style.easyxf(num_format_str='￥#,##0.00') # set cell style for RMB currency
+titleStyle = xlwt.Style.easyxf(strg_to_parse='font: bold on')
 sheet0 = outFile.add_sheet('Sheet1', False) # add a new sheet1 into output file
 
+rowsWr = 1 # how many rows written
+sheet0.write(r=0, c=0, label='借款人', style=titleStyle)
+sheet0.write(r=0, c=1, label='当期应还利息', style=titleStyle)
+sheet0.write(r=0, c=2, label='还款日', style=titleStyle)
+
 listFiles = os.listdir(wdir) # list out all files under working folder
-currencyStyle = xlwt.Style.easyxf(num_format_str='￥#,##0.00')
 for fl in listFiles:
     print('processing file',fl, '...')
     filename, fileext = os.path.splitext(fl)
@@ -47,7 +52,7 @@ for fl in listFiles:
             sheet0.write(r=rowsWr, c=0, label=name)
             
             interestValue = sheet.cell(3+rowsRd, 2).value
-            sheet0.write(r=rowsWr, c=1, label=round(interestValue, 2), style=currencyStyle)
+            sheet0.write(r=rowsWr, c=1, label=round(interestValue, 2), style=rmbStyle)
             
             if (sheet.cell(3+rowsRd, 6).ctype == 3): # cell value is date type
                 dateValue = xlrd.xldate_as_tuple(sheet.cell(3+rowsRd, 6).value, workbook.datemode)
@@ -59,6 +64,6 @@ for fl in listFiles:
             rowsWr += 1
             rowsRd += 1
 
-if (rowsWr > 0):
+if (rowsWr > 1):
     print('Process complete, save output file as merged_file.xls')
     outFile.save(outputFilename)
